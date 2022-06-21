2eb053f6cb2c0be8a92dab6c1b8bd0dc59418e69
toghrulvg <toghrulvg@code.edu.az>
2022-06-21 06:37:35 +0400

    added

---
+++ Methods/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace NewMethods
+   {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1)Dividing(42);
+            //(2)CountOdd(10 , 13);
+            //(3)SumBtwEvens(2, 14);
+
+            //(4)int[] arr = {1, 2, 3, 4, 5, 6 , 7 ,8, 9 };
+            //SumOfOddsArr(arr);
+
+            //(5)int[] arr = { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+            //CountOfEven(arr);
+
+            //(6)CompositeOrPrime(17);
+
+        }
+
+
+        //static void Dividing(int n)
+        //{
+
+        //    if (n % 3 == 0 && n % 7 == 0)
+        //    {
+        //        Console.WriteLine("Eded 3 ve 7'e qaliqsiz bolunur");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("duzgun eded daxil edin");
+        //    }
+        //}
+
+
+        //static void CountOdd(int n , int m)
+        //{ 
+
+        //    if (n % 2 == 0 && m % 2 == 0)
+        //    {
+        //        Console.WriteLine(n + m);
+        //    }
+        //    else Console.WriteLine("Ededlerden biri ve ya her ikisi tek ededdir");
+        //}
+
+        //static void SumBtwEvens(int n , int m)
+        //{
+        //    int cem = 0;
+        //    for (int i = n; i < m; i++)
+        //    {
+        //        if (i % 2 == 0)
+        //            cem += i;
+        //    }
+        //    Console.WriteLine("verilmish n<m araligindaki cut ededlerin cemi=" + cem);
+        //}
+
+        //static void SumOfOddsArr(int[] arr)
+        //{
+
+        //    int cem = 0;
+
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (i % 2 == 0)
+        //            cem += arr[i];
+        //    }
+
+        //    Console.WriteLine("verilmish array'daki tek ededlerin cemi="
+        //                           + cem);
+
+        //}
+
+        //static void CountOfEven(int[] arr)
+        //{
+        //    int cut = 0;
+
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (i % 2 == 0)
+        //            cut++;
+        //    }
+        //    Console.WriteLine("cut ededlerin sayi=" + cut);
+        //}
+
+
+        //static void CompositeOrPrime(int n)
+        //{
+        //    int count = 0;
+        //    {
+        //        for (int i = 1; i <= n; i++)
+        //        {
+        //            if (n % i == 0)
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        if (count > 2)
+        //        {
+        //            Console.WriteLine("eded murekkebdir");
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("eded sadedir");
+        //        }
+
+        //    }
+
+
+        //}
+
+
+
+    }
+}
+
+
+
+
+
+
+   
